bba12784d027dd9fc009ba69c8cdeb40e0669168
Hassaan Siddiqui <59255860+has-sidd@users.noreply.github.com>
2021-11-23 01:34:16 +0500

    Add files via upload

---
+++ Week9/Q5.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Q5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        double num1, num2;
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            num1 = Convert.ToDouble(textBox1.Text);
+            num2 = Convert.ToDouble(textBox2.Text);
+            textBox3.Text = (num1 - num2).ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            num1 = Convert.ToDouble(textBox1.Text);
+            num2 = Convert.ToDouble(textBox2.Text);
+            textBox3.Text = (num1 * num2).ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            num1 = Convert.ToDouble(textBox1.Text);
+            num2 = Convert.ToDouble(textBox2.Text);
+            textBox3.Text = (num1 / num2).ToString();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            num1 = Convert.ToDouble(textBox1.Text);
+            num2 = Convert.ToDouble(textBox2.Text);
+            textBox3.Text = (num1 + num2).ToString();
+        }
+    }
+}
