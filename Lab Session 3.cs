2b8d58225f6b6b1209a4a391eae2ada8ae12e3cf
Hassaan Siddiqui <59255860+has-sidd@users.noreply.github.com>
2021-11-23 01:22:31 +0500

    Add files via upload

---
+++ Lab Session 3.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_Session_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int deci = 1;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+            string bin = Convert.ToString(deci, 2);
+            string hexa = Convert.ToString(deci, 16);
+            textBox1.Text += deci + "\t" + bin+ "\t" + hexa + Environment.NewLine;
+            deci++;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.Text += "Decimal " + "Binary " + "Hexa" + Environment.NewLine;
+        }
+    }
+}
